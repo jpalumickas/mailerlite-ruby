d2ba1be2cfcba48f626c29047ec6afb190b0d7cf
Justas Palumickas <jpalumickas@gmail.com>
2016-04-17 19:32:29 +0300

    Update spec helper, add first texture

--- spec/mailerlite_spec.rb
+++ spec/mailerlite_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
-describe Mailerlite do
+describe MailerLite do
   it 'has a version number' do
-    expect(Mailerlite::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(MailerLite::VERSION).not_to be nil
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,26 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'simplecov'
+require 'coveralls'
+require 'codeclimate-test-reporter'
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter,
+  CodeClimate::TestReporter::Formatter
+]
+
+SimpleCov.start
+
 require 'mailerlite'
+require 'webmock/rspec'
+
+WebMock.disable_net_connect!(allow: ['coveralls.io', 'codeclimate.com'])
+
+def fixtures_dir
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(file)
+  File.join(fixtures_path, "#{file}.json")
+end
