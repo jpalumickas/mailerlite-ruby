89d86eae3d769dbbadd40eda91253e1aeef5b226
Justas Palumickas <jpalumickas@gmail.com>
2016-04-21 22:44:51 +0300

    Add support for configuring custom user agent and api endpoint

--- lib/mailerlite/configuration.rb
+++ lib/mailerlite/configuration.rb
@@ -1,20 +1,27 @@
 module MailerLite
   # A class responsible for all configurations.
   class Configuration
-    # Default API endpoint
+    # Default API endpoint.
     API_ENDPOINT = 'https://app.mailerlite.com/api/v1'.freeze
 
-    # Default User Agent header string
+    # Default User Agent header string.
     USER_AGENT = "MailerLite Ruby v#{MailerLite::VERSION}".freeze
 
     attr_accessor :api_key
+    attr_writer :url, :user_agent
 
+    # Takes url provided from configuration or uses default one.
+    #
+    # @return [String] An API Endpoint url which will be used for connection.
     def url
-      API_ENDPOINT
+      @url || API_ENDPOINT
     end
 
+    # Takes user agent from configuration or uses default one.
+    #
+    # @return [String] User agent which will be used for connection headers.
     def user_agent
-      USER_AGENT
+      @user_agent || USER_AGENT
     end
   end
 end
--- spec/mailerlite/configuration_spec.rb
+++ spec/mailerlite/configuration_spec.rb
@@ -5,14 +5,24 @@ describe MailerLite::Configuration do
   let(:version) { MailerLite::VERSION }
 
   describe '#url' do
-    it 'has correct value' do
+    it 'has correct default value' do
       expect(config.url).to eq('https://app.mailerlite.com/api/v1')
+    end
+
+    it 'has correct custom value' do
+      config.url = 'test_url'
+      expect(config.url).to eq('test_url')
     end
   end
 
   describe '#user_agent' do
     it 'has correct value' do
       expect(config.user_agent).to eq("MailerLite Ruby v#{version}")
+    end
+
+    it 'has correct custom value' do
+      config.user_agent = 'My user agent'
+      expect(config.user_agent).to eq('My user agent')
     end
   end
 end
